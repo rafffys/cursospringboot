01aba764932b15427f1e3fbdc4531ae6e3dff9fd
Rafa Rodrigues <rafffys@gmail.com>
2019-02-26 13:08:10 -0300

    Item 10.3 até item 5)

--- src/main/java/br/com/caelum/forum/service/infra/ForumMailService.java
+++ src/main/java/br/com/caelum/forum/service/infra/ForumMailService.java
@@ -1,11 +1,13 @@
 package br.com.caelum.forum.service.infra;
 
+import br.com.caelum.forum.infra.NewReplyMailFactory;
 import br.com.caelum.forum.model.Answer;
 import br.com.caelum.forum.model.topic.domain.Topic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -15,18 +17,25 @@ public class ForumMailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Autowired
+    private NewReplyMailFactory newReplyMailFactory;
+
     @Async
-    public void sendNewReplyMail(Answer answer) {
+    public void sendNewReplyMailAsync(Answer answer) {
         Topic answeredTopic = answer.getTopic();
 
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(answeredTopic.getOwnerEmail());
-        message.setSubject("Novo comentário em: " + answeredTopic.getShortDescription());
-        message.setText("Olá, " + answeredTopic.getOwnerEmail() + "!\n\n" +
-                "Há uma nova mensagem no forum: " + answer.getOwnerName() + " comentou no tópico: " +
-                answeredTopic.getShortDescription());
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+
+            messageHelper.setTo(answeredTopic.getOwnerEmail());
+            messageHelper.setSubject("Novo comentário em: " + answeredTopic.getShortDescription());
+            String messageContent = this.newReplyMailFactory.generateNewReplyMailContent(answer);
+            messageHelper.setText(messageContent, true);
+
+        };
+
         try {
-            mailSender.send(message);
+            mailSender.send(messagePreparator);
         } catch (MailException e) {
             throw new MailServiceException("Não foi possível enviar o email. ", e);
         }
