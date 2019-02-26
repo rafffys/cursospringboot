74384cc3d4637ce033caad8c20b28672a0b13293
Rafa Rodrigues <rafffys@gmail.com>
2019-02-26 12:38:47 -0300

    Correção item 10.2

--- src/main/java/br/com/caelum/forum/service/NewReplyProcessorService.java
+++ src/main/java/br/com/caelum/forum/service/NewReplyProcessorService.java
@@ -1,10 +1,8 @@
 package br.com.caelum.forum.service;
 
 import br.com.caelum.forum.model.Answer;
-import br.com.caelum.forum.model.topic.domain.Topic;
 import br.com.caelum.forum.repository.AnswerRepository;
 import br.com.caelum.forum.service.infra.ForumMailService;
-import br.com.caelum.forum.service.infra.MailServiceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,12 +23,6 @@ public class NewReplyProcessorService {
 
     public void execute(Answer answer) {
         this.answerRepository.save(answer);
-
-        try {
-            this.forumMailService.sendNewReplyMail(answer);
-        } catch (MailServiceException e) {
-            Topic answeredTopic = answer.getTopic();
-            logger.error("Não foi possível notificar o usuário {} enviando email para {}", answeredTopic.getOwnerName(), answeredTopic.getOwnerEmail());
-        }
+        this.forumMailService.sendNewReplyMail(answer);
     }
 }
