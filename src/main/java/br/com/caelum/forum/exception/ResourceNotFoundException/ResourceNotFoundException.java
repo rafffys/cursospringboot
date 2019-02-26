8a92f7a91edf04dfa09f1bca0a165359433b6512
Rafa Rodrigues <rafffys@gmail.com>
2019-02-26 09:46:29 -0300

    Sétimo dia - Parte 1

--- src/main/java/br/com/caelum/forum/exception/ResourceNotFoundException/ResourceNotFoundException.java
+++ src/main/java/br/com/caelum/forum/exception/ResourceNotFoundException/ResourceNotFoundException.java
@@ -2,6 +2,6 @@ package br.com.caelum.forum.exception.ResourceNotFoundException;
 
 public class ResourceNotFoundException extends Exception {
     public ResourceNotFoundException(String s) {
-
+        super(s);
     }
 }
